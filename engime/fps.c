975379ba8eb719f91f9f0cc178b7ba758792c464
tokyfy <tokyfydev@gmail.com>
2024-07-23 16:03:43 +0300

    refactoring

--- engime/fps.c
+++ engime/fps.c
@@ -6,17 +6,17 @@
 /*   By: franaivo <franaivo@student.42antananariv>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 09:01:52 by franaivo          #+#    #+#             */
-/*   Updated: 2024/07/23 15:33:01 by franaivo         ###   ########.fr       */
+/*   Updated: 2024/07/23 15:57:31 by franaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "engime.h"
 
 uint64_t	gettimeofday_ms(void)
 {
-	static struct timeval	tv;
+	static struct timeval	t;
 
-	gettimeofday(&tv, NULL);
-	return ((tv.tv_sec * (uint64_t)1000) + (tv.tv_usec / 1000));
+	gettimeofday(&t, NULL);
+	return ((t.tv_sec * (uint64_t)1000) + (t.tv_usec / 1000));
 }
 
 uint64_t	timestamp_in_ms(void)
